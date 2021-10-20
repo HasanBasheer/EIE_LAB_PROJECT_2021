1df82ac61b5742ee9d76e375c1b234d35223536b
Hazey <hassaanhmd@gmail.com>
2021-10-20 20:34:29 +0200

    download csv + some metrics

--- controllers/search.controller.js
+++ controllers/search.controller.js
@@ -1,5 +1,38 @@
 const SearchObject = require('../models/search.model.js')
+const json2csv = require('json-2-csv')
 
+exports.downloadQuery = function (req, res) {
+  const newSearchObject = new SearchObject(req.body)
+  // console.log(newSearchObject)
+
+  SearchObject.createQuery(newSearchObject, function (err, result) {
+    if (err) {
+      res.status(400).send({ error: true, message: err })
+      return
+    }
+
+    // const json2csv = new Parser({ fields });
+    json2csv.json2csv(result, (err, csv) => {
+      if (err) {
+        throw err
+      }
+      console.log(csv)
+      res.header('Content-Type', 'text/csv')
+      res.attachment('result.csv')
+      return res.send(csv)
+    })
+
+    // converter.json2csv(res, (err, csv) => {
+    //   if (err) {
+    //     throw err
+    //   }
+
+    //   // print CSV string
+    //   console.log(csv)
+    // })
+    // res.json(result)
+  })
+}
 exports.createQuery = function (req, res) {
   const newSearchObject = new SearchObject(req.body)
   // console.log(newSearchObject)
